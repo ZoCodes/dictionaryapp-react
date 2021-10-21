e6b6fa63f9f966650531991c0d482adea5c3161a
ZoCodes <zoeblogg91@gmail.com>
2021-10-21 20:01:45 +0100

    Sorted React files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello it is ZoCodes</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
