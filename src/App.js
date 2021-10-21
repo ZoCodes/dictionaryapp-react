aba2fe579f2aa048e330c17e9d4a1d48b2262369
ZoCodes <zoeblogg91@gmail.com>
2021-10-21 21:40:56 +0100

    Added Dictionary component and a matching CSS file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
---
+++ src/Dictionary.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  return <div className="Dictionary">Hello from Dictionary</div>;
+}
