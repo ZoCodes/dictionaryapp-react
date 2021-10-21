7716629af2059b701e5c8e8fa221d329e0eeb3d7
ZoCodes <zoeblogg91@gmail.com>
2021-10-21 21:33:23 +0100

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello it is ZoCodes</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Don't gobblefunk around with words....</h1>
+          <h2> - Roald Dahl</h2>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center">Coded by ZoCodes</footer>
+      </div>
     </div>
   );
 }
