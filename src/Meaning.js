9f0b1b4a1c308c36ad27aec964c8c10909b0d9ca
ZoCodes <zoeblogg91@gmail.com>
2021-10-23 17:54:11 +0100

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Meaning(props) {
-  return "Hello from Meaning";
+  console.log(props.meaning);
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>;
+      <p>{props.meaning.definitions[0].definition}</p>
+      <p>{props.meaning.definitions[0].example}</p>
+    </div>
+  );
 }
