198ce8207ee235985eb6139073e1a4c9dd57854b
ZoCodes <zoeblogg91@gmail.com>
2021-10-23 18:02:07 +0100

    refactored meaning code

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,8 +5,18 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>;
-      <p>{props.meaning.definitions[0].definition}</p>
-      <p>{props.meaning.definitions[0].example}</p>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
     </div>
   );
 }
