f68647304575fd6cfa3b96dc8363363807280064
ZoCodes <zoeblogg91@gmail.com>
2021-10-23 12:26:43 +0100

    added axios

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,8 +1,13 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyphrase, setKeyphrase] = useState("");
+
+  function handleApiResponse(response) {
+    console.log(response);
+  }
 
   function handleWordSearch(event) {
     setKeyphrase(event.target.value);
@@ -11,6 +16,9 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyphrase} definition`);
+
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_US/hello`;
+    axios.get(apiURL).then(handleApiResponse);
   }
 
   return (
