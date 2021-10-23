b8cba6c1b1eb59e423104d51c60ab0eb879cf3a5
ZoCodes <zoeblogg91@gmail.com>
2021-10-23 13:59:01 +0100

    created a component for results of definition

---
+++ src/Definition.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Definition() {
+  return <div className="Definition"></div>;
+}
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+
+import Definition from "./Definition";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyphrase, setKeyphrase] = useState("");
+  let [definition, setDefinition] = useState(null);
 
   function handleApiResponse(response) {
     console.log(response.data[0]);
+    console.log(response.data[0].meaning[0].definitions[0].definition);
   }
 
   function handleWordSearch(event) {
@@ -18,7 +22,7 @@ export default function Dictionary() {
 
     //documentation: https://dictionaryapi.dev/ //
 
-    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en_UK/${keyphrase}`;
+    let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyphrase}`;
     axios.get(apiURL).then(handleApiResponse);
   }
 
@@ -27,6 +31,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleWordSearch} />
       </form>
+      <Definition />
     </div>
   );
 }
