cd620d10bc34a89cef949c9b6f9824d1258d474d
ZoCodes <zoeblogg91@gmail.com>
2021-10-24 13:48:32 +0100

    added audio file for {props.phonetic.audio}

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,13 +9,11 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              <strong>Definition:</strong> {definition.definition}
-              <br />
-              <strong>Example:</strong> <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms} />
-            </p>
+            <strong>Definition:</strong> {definition.definition}
+            <br />
+            <strong>Example:</strong> <em>{definition.example}</em>
+            <br />
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Phonetics(props) {
-  return <div className="Phonetics">Hello from Phonetics</div>;
+  return (
+    <div className="Phonetics">
+      <a href={props.phonetic.audio}>Listen</a>
+      <br />
+      {props.phonetic.text}
+    </div>
+  );
 }
