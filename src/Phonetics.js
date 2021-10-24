ac2daed4c91efcc669aaa69e3b3b4a90bccdda7e
ZoCodes <zoeblogg91@gmail.com>
2021-10-24 13:50:05 +0100

    added a comment to remind self to add image rather than word 'listen'

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export default function Phonetics(props) {
+  // add an icon of a listen image to play sound rather than the word listen
   return (
     <div className="Phonetics">
       <a href={props.phonetic.audio}>Listen</a>
