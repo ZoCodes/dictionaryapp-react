d6a11bf2ddf826c3e84dccd4ac2b4226c3409842
ZoCodes <zoeblogg91@gmail.com>
2021-10-24 13:10:29 +0100

    Added unordered list

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  console.log(props.synonyms);
   if (props.synonyms) {
-    return <div className="Synonyms">Hi from Synonyms</div>;
+    return (
+      <div className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <ul>
+              <li key={index}>{synonym}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
